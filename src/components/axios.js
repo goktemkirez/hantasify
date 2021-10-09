61ede848e56748a53daddc440e55b96eda6ae338
Baris Hantas <barhantas@gmail.com>
2021-10-10 00:48:18 +0300

    add some comments

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY =
-  "BQD8tz_e6OrLbbJM9ySdqt7g3WlNXDlJ81pMZ11e2KskS1L7yfw7ZNNiSiMLaDc4HuAR9jQIpCTN6zBsgmFtZ_h52G9JYoSWO_sOempR67uFGtu2DxAvwHbN0updsfx_GwVpSkBHdPqLrqmYPgPtU8q2I7SljpM8NAnB6RKpIdvJY8SEldIjQBEq7iY";
+  "BQAb0_AbBE6uRl6twPA9mmVpobp7W6V_SI8DHfQANy9aPqSCipOm2-jlJXcF2BMzSN8jGIKqIqpgQP5IVgyy--TYLOTS9wap46lzCMZYnCHih9bzbbr4SPKYBaXrClddvaIcxJpTRDZIZpLCr9eNWmvexV1Q-ASkY6IAfaS07QBh_gwSGWCp2Jyc53LxKbiIkMSd4WU6FDEMXL4UQ18CnzO2Grqyc1qVUxxUDcqDj_AOEkLlZTLKvaL6v4BE6VJUmv0M6pFWcegs8iZ4aBnKlDxcPa6WcBWAjjiCgQ";
 
 const authAxios = axios.create({
   baseURL: "https://api.spotify.com/v1",
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
 import authAxios from "../components/axios";
-import { Box, Button, Container } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Box, Button, Container, makeStyles } from "@material-ui/core";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 import ProfileCard from "../components/profileCard";
 
 function Profile() {
   const classes = useStyles();
-  const [loading, setLoading] = useState(false);
-  const [profileData, setProfileData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [profileData, setProfileData] = useState();
 
   useEffect(() => {
     console.log("component ilk yüklendiğinde çalışırım");
@@ -18,44 +18,62 @@ function Profile() {
   const getUserDetails = async () => {
     try {
       setLoading(true);
-
       const result = await authAxios.get(`/me`);
       console.log(result.data);
       setProfileData(result.data);
     } catch (error) {
       console.log("error", error);
     } finally {
-      setLoading(false);
+      //2 saniye sonra loadingi false çeksin de aşağıdaki loading componentlerini gör diye koydum yoksa direk setLoading(false) yeterli
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
     }
   };
 
   return (
     <div>
       <Container maxWidth="sm" className={classes.containerStyle}>
-        <ProfileCard
-          Product={profileData.product}
-          DisplayName={profileData.display_name}
-          Email={profileData.email}
-          Followers={"1"}
-        />
-        <Box component="div" m={1} className={classes.boxStyle}>
-          <Button variant="contained" />
-          <h1>PROFİLE PAGE</h1>
-          <h5>{profileData.country}</h5>
-          <h5>{profileData.display_name}</h5>
-          <h5>{profileData.email}</h5>
-          <h5>{profileData.href}</h5>
-          <h5>Followers :{"1"}</h5>
-          <h5>{profileData.product}</h5>
+        {loading ? (
+          <Box>
+            <Skeleton animation="wave" width="240" />
+            <Skeleton animation="wave" width="240" />
+            <Skeleton animation="wave" width="240" />
+            <Skeleton animation="wave" width="240" />
+          </Box>
+        ) : (
+          <>
+            <ProfileCard
+              Product={profileData.product}
+              DisplayName={profileData.display_name}
+              Email={profileData.email}
+              Followers={"1"}
+            />
+            <Box component="div" m={1} className={classes.boxStyle}>
+              <Button variant="contained" />
+              <h1>PROFİLE PAGE</h1>
+              <h5>{profileData.country}</h5>
+              <h5>{profileData.display_name}</h5>
+              <h5>{profileData.email}</h5>
+              <h5>{profileData.href}</h5>
+              {/*loading ile ilgili bi örnek koydum daha iyisini yapabiliriz konsept olarak kafanda olsun */}
 
-          {/* GELEN DATADA FOLLOWERS İÇİNDE TOTAL DATASINI NASIL ÇEKİCEM. BARIŞ */}
-          {/* {profileData.map((rowData, i) => (
-            <div>
-              <h4>{rowData.email}</h4>
-              <h4>{rowData.followers.total}</h4>
-            </div>
-          ))} */}
-        </Box>
+              {/* Followers countı bu şekilde alabilirsin eminim sen de bunu yaptın ama bir şekilde hata aldın. Burdaki problem şu profile.followers dediğinde eğer o an profile undefined ise kızar. cannot read property xxx of undefined diye bu yüzden obje içi proportylere erişirken dikkatli olmak lazım. soru işareti ile bir kullanım var bir de varsa diye x && x.profile && x.profile.name gibi*/}
+              {/* 1 tane lock dosyası tut ya package.lock ya da yarn.lock bu lockların ne işe yaradığını okumanı tavsiye ederim */}
+
+              <h5>{`Followers ${profileData?.followers?.total}`}</h5>
+              <h5>{profileData.product}</h5>
+
+              {/* GELEN DATADA FOLLOWERS İÇİNDE TOTAL DATASINI NASIL ÇEKİCEM. BARIŞ */}
+              {/* {profileData.map((rowData, i) => (
+              <div>
+                <h4>{rowData.email}</h4>
+                <h4>{rowData.followers.total}</h4>
+              </div>
+            ))} */}
+            </Box>
+          </>
+        )}
       </Container>
     </div>
   );
