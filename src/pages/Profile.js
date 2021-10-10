18a64988edaf34e13e28ac47ccedaf0040a9ca08
Orçun Göktem Kirez <ogkirez@gmail.com>
2021-10-10 16:12:43 +0300

    loading ok

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import authAxios from "../components/axios";
-import { Box, Button, Container } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Box, Button, Container, makeStyles } from "@material-ui/core";
+import { Skeleton } from "@material-ui/lab";
 
 import ProfileCard from "../components/ProfileCard";
 
@@ -25,37 +25,32 @@ function Profile() {
     } catch (error) {
       console.log("error", error);
     } finally {
-      setLoading(false);
+      // setLoading(false);
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
     }
   };
 
   return (
     <div>
       <Container maxWidth="sm" className={classes.containerStyle}>
-        <ProfileCard
-          product={profileData.product}
-          displayName={profileData.display_name}
-          email={profileData.email}
-          followers={"1"}
-        />
-        <Box component="div" m={1} className={classes.boxStyle}>
-          <Button variant="contained" />
-          <h1>PROFİLE PAGE</h1>
-          <h5>{profileData.country}</h5>
-          <h5>{profileData.display_name}</h5>
-          <h5>{profileData.email}</h5>
-          <h5>{profileData.href}</h5>
-          <h5>Followers :{"1"}</h5>
-          <h5>{profileData.product}</h5>
-
-          {/* GELEN DATADA FOLLOWERS İÇİNDE TOTAL DATASINI NASIL ÇEKİCEM. BARIŞ */}
-          {/* {profileData.map((rowData, i) => (
-            <div>
-              <h4>{rowData.email}</h4>
-              <h4>{rowData.followers.total}</h4>
-            </div>
-          ))} */}
-        </Box>
+        {loading ? (
+          <Box sizeHeight="50">
+            <Skeleton animation="pulse" width="240">
+              loading
+            </Skeleton>
+          </Box>
+        ) : (
+          <>
+            <ProfileCard
+              product={profileData.product}
+              displayName={profileData.display_name}
+              email={profileData.email}
+              followers={`Followers ${profileData?.followers?.total}`}
+            />
+          </>
+        )}
       </Container>
     </div>
   );
